fa8deb996e0b4c895bc4106b4e1eb8cb5c7174bf
Sarah Lopez <slopez323@gmail.com>
2022-09-27 15:21:14 -0400

    context

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -3,19 +3,13 @@ import { productList } from "../../mockData";
 import Layout from "../layout/Layout";
 import ProductDisplay from "../ProductDisplay";
 
-function HomePage(props) {
-  const { shoppingCart, setShoppingCart } = props;
-
+function HomePage() {
   return (
     <Layout>
       <Box display="flex" flexDirection="column" alignItems="center">
         {productList.map((product) => (
           <Box mb={6} key={product.id}>
-            <ProductDisplay
-              productData={product}
-              shoppingCart={shoppingCart}
-              setShoppingCart={setShoppingCart}
-            />
+            <ProductDisplay productData={product} />
           </Box>
         ))}
       </Box>
